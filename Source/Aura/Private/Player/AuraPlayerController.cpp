8bf5ff39787a41d5c873d80ec1e8f72d57e78a53
FourNN <FlynnKUN@outlook.com>
2024-12-07 20:41:00 +0800

    配置增强输入系统

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,22 @@
 
 #include "Player/AuraPlayerController.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates=true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);//检测是否为野指针，是的话游戏崩溃
+	UEnhancedInputLocalPlayerSubsystem*Subsystem=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext,0);//添加映射，设置此映射优先级为0
+	bShowMouseCursor=true;
+	DefaultMouseCursor=EMouseCursor::Default;//设置鼠标指针样式为默认值
+	FInputModeGameAndUI InputModeData;//创建输入数据
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);//鼠标不会锁定到视口
+	InputModeData.SetHideCursorDuringCapture(false);//设置鼠标进入屏幕时不会被隐藏
+	SetInputMode(InputModeData);
+}
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -1,17 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
 
-/**
- * 
- */
+
+class UInputMappingContext;//为了使用增强输入，必须显式声明，同时在.build.cs中添加“EnhancedInput”
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+private:
+	TObjectPtr<UInputMappingContext>AuraContext;
 };
