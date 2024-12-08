33d1ad6b3329048d2a5cf42b4cb7cf1e9fafc9da
FourNN <FlynnKUN@outlook.com>
2024-12-08 14:17:38 +0800

    增加敌人蓝图基类_使用后期处理体积完成Actor高亮显示

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,27 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
+
 void AAuraEnemy::HighLightActor()
 {
-	bHighLight=true;
+
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHightLightActor()
 {
-	bHighLight=false;
+
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+
 }
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -15,9 +15,8 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase,public IEnemyInterface
 {
 	GENERATED_BODY()
 public:
+	AAuraEnemy();
 	virtual void HighLightActor() override;
 	virtual void UnHightLightActor() override;
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighLight=false;
-	
+
 };
