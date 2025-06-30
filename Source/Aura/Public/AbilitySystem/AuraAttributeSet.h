6ac61a07a24df545179f79a365b72ad39e740298
FourNN <FlynnKUN@outlook.com>
2025-06-30 22:32:13 +0800

    为属性集添加基本属性

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,34 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+
+	UAuraAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Health)
+	FGameplayAttributeData Health;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth)const;
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)const;
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana)
+	FGameplayAttributeData Mana;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana)const;
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)const;
 	
 };
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -19,15 +19,21 @@ class AURA_API AAuraCharacterBase : public ACharacter,public IAbilitySystemInter
 public:
 	
 	AAuraCharacterBase();
+	
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
 	UAttributeSet* GetAttributeSet()const{return AttributeSet;}
+	
 protected:
 
 	virtual void BeginPlay() override;
+	
 	UPROPERTY(EditAnywhere,Category="Combat")
 	TObjectPtr<USkeletalMeshComponent>Weapon;
+	
 	UPROPERTY()
 	TObjectPtr<UAbilitySystemComponent>AbilitySystemComponent;
+	
 	UPROPERTY()
 	TObjectPtr<UAttributeSet>AttributeSet;
 
