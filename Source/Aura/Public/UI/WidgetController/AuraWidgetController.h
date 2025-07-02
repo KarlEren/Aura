c6a2650bf03ebc5a8b84285aa0eb40d26b8eca32
FourNN <FlynnKUN@outlook.com>
2025-07-02 22:25:36 +0800

    新建UI父类资产、UI父类和UI管理器

---
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraWidgetController.generated.h"
+
+class UAttributeSet;
+class UAbilitySystemComponent;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+protected:
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<APlayerController> playerController;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UAttributeSet> AttributeSet;
+	
+};
